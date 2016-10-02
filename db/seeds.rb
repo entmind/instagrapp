b17a5b0d07b80137fa9c008035744e461017083f
entmind <entmind@gmail.com>
2016-10-02 15:50:01 +0000

    seed-dekita-kana...

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,24 @@
   email = Faker::Internet.email
   password = "password"
   name = Faker::Name.last_name
+  uid = Faker:: Number.number(10)
+  provider = "twitter"
   User.create!(email: email,
     password: password,
     password_confirmation: password,
-    name: name
+    name: name,
+    uid: uid,
+    provider: provider
     )
 end
 
 n = 1
+pic_path = File.join(Rails.root, "app/assets/images/pic1.jpg")
 while n <= 100
-  Picture.create(
+  Picture.create!(
     title: "あああ",
-    user_id: n
+    user_id: n,
+    pic: File.new(pic_path)
   )
   n = n + 1
 end
